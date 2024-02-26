670202b84fd7f25bab95b7cb89b409ac00d90f21
fpem3309 <fpem3309@gmail.com>
2024-02-26 17:48:32 +0900

    Bronze3(sec04) 하는중

---
+++ src/bronze4_5/sec04/ex07_27866.java
@@ -0,0 +1,13 @@
+package bronze4_5.sec04;
+
+import java.util.Scanner;
+
+public class ex07_27866 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String S = sc.nextLine();
+        int i = sc.nextInt();
+
+        System.out.println(S.charAt(i-1));
+    }
+}
