4e3b677c4ac48c456b617313e1a3827234377a96
fpem3309 <alsrhks2154@gmail.com>
2024-05-14 22:58:56 +0900

    [greedy]1026

---
+++ src/algorithm/greedy/ex06_1026_보물.java
@@ -0,0 +1,40 @@
+package algorithm.greedy;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * https://www.acmicpc.net/problem/1026
+ */
+public class ex06_1026_보물 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] a = new int[n];
+        int[] b = new int[n];
+        int result = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(a);
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            b[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(b);
+
+        for (int i = 0; i < n; i++) {
+            result += a[i] * b[n - i - 1];
+        }
+
+        System.out.println(result);
+    }
+}
